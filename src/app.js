9e476dc75d4182d747abd689785a12c1ca7e6ef5
Kaivela <kaivel100@gmail.com>
2023-11-10 14:17:44 +0100

    01.2-Read-From-BBD

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
-
 const movieControllers = require("./controllers/movieControllers");
+const userControllers = require("./controllers/userControllers");
+
+const welcome = (req, res) => {
+  res.send("Welcome to my Express_Quests app");
+};
+
+app.get("/", welcome);
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
+app.get("/api/users", userControllers.getUsers);
+app.get("/api/users/:id", userControllers.getUserById);
 
 module.exports = app;
