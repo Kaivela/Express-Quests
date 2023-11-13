6411f329e3e6747a3660373beaa5d19f7b4f3cda
Kaivela <kaivel100@gmail.com>
2023-11-13 11:51:38 +0100

    02-post-method

--- src/app.js
+++ src/app.js
@@ -13,8 +13,10 @@ app.get("/", welcome);
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.post("/api/movies", movieControllers.postMovie);
 
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
+app.post("/api/users", userControllers.postUser);
 
 module.exports = app;
